dade37e5e8fd9d32b2591cadce36835e5e299d71
Parris Varney <pvarney@copatient.com>
2015-08-12 10:29:04 -0400

    Wait for url attribute resolution before attempting to retrieve PDF

--- src/ng-pdf-viewer.js
+++ src/ng-pdf-viewer.js
@@ -1,35 +1,43 @@
-angular.module('pdf-viewer', []).directive('pdfViewer', [function() {
+angular.module('pdf-viewer', []).directive('pdfViewer', ['$q', function($q) {
     return {
         restrict: 'E',
+        scope: {
+            url: '@'
+        },
         link: function(scope, element, attrs) {
-            PDFJS.getDocument(attrs.url).then(function(pdf) {
-                var getPages = function(pageNum, pageCount) {
-                    var canvas = document.createElement('canvas');
+            scope.$watch('url', function(url) {
+                if(!url) {
+                    return;
+                }
+
+                PDFJS.getDocument(scope.url).then(function(pdf) {
+                    var getPages = function(pageNum, pageCount) {
+                        var canvas = document.createElement('canvas');
                         canvas.className = attrs.class ? attrs.class : '';
-                    element[0].appendChild(canvas);
-                    pdf.getPage(pageNum).then(function(page) {
-                        var viewport       = page.getViewport(1),
-                            scale          = attrs.width ? (attrs.width / viewport.width) : 1,
-                            viewportScaled = page.getViewport(scale),
-                            context        = canvas.getContext('2d'),
-                            renderContext = {
-                                canvasContext: context,
-                                viewport:      viewportScaled
-                            };
+                        element[0].appendChild(canvas);
+                        pdf.getPage(pageNum).then(function(page) {
+                            var viewport       = page.getViewport(1),
+                                scale          = attrs.width ? (attrs.width / viewport.width) : 1,
+                                viewportScaled = page.getViewport(scale),
+                                context        = canvas.getContext('2d'),
+                                renderContext = {
+                                    canvasContext: context,
+                                    viewport:      viewportScaled
+                                };
 
-                        canvas.height = viewportScaled.height;
-                        canvas.width  = viewportScaled.width;
+                            canvas.height = viewportScaled.height;
+                            canvas.width  = viewportScaled.width;
 
-                        page.render(renderContext);
+                            page.render(renderContext);
 
-                        if (pageNum  < pageCount) {
-                            getPages(pageNum + 1, pageCount);
-                        }
-                    });
+                            if (pageNum  < pageCount) {
+                                getPages(pageNum + 1, pageCount);
+                            }
+                        });
+                    };
 
-                };
-
-                getPages(1, pdf.numPages);
+                    getPages(1, pdf.numPages);
+                });
             });
         }
     };

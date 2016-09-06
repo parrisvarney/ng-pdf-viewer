20e2f2e68c64a301324dbc53c5d21046a6b51dd2
John Blair <john.blair1@gmail.com>
2016-09-06 14:42:29 +0100

    clean canvas before adding new one

--- src/ng-pdf-viewer.js
+++ src/ng-pdf-viewer.js
@@ -1,44 +1,54 @@
 angular.module('pdf-viewer', []).directive('pdfViewer', ['$q', function($q) {
-    return {
-        restrict: 'E',
-        scope: {
-            url: '@'
-        },
-        link: function(scope, element, attrs) {
-            scope.$watch('url', function(url) {
-                if(!url) {
-                    return;
-                }
-
-                PDFJS.getDocument(scope.url).then(function(pdf) {
-                    var getPages = function(pageNum, pageCount) {
-                        var canvas = document.createElement('canvas');
-                        canvas.className = attrs.class ? attrs.class : '';
-                        element[0].appendChild(canvas);
-                        pdf.getPage(pageNum).then(function(page) {
-                            var viewport       = page.getViewport(1),
-                                scale          = attrs.width ? (attrs.width / viewport.width) : 1,
-                                viewportScaled = page.getViewport(scale),
-                                context        = canvas.getContext('2d'),
-                                renderContext = {
-                                    canvasContext: context,
-                                    viewport:      viewportScaled
-                                };
-
-                            canvas.height = viewportScaled.height;
-                            canvas.width  = viewportScaled.width;
-
-                            page.render(renderContext);
-
-                            if (pageNum  < pageCount) {
-                                getPages(pageNum + 1, pageCount);
-                            }
-                        });
-                    };
-
-                    getPages(1, pdf.numPages);
-                });
-            });
+  return {
+    restrict: 'E',
+    scope: {
+      url: '@'
+    },
+    link: function(scope, element, attrs) {
+      scope.$watch('url', function(url) {
+        if(!url) {
+          return;
         }
-    };
+
+        PDFJS.getDocument(scope.url).then(function(pdf) {
+
+          var getPages = function(pageNum, pageCount) {
+            var canvas = document.createElement('canvas');
+            canvas.className = attrs.class ? attrs.class : '';
+            element[0].appendChild(canvas);
+            pdf.getPage(pageNum).then(function(page) {
+              var viewport       = page.getViewport(1),
+                  scale          = attrs.width ? (attrs.width / viewport.width) : 1,
+                  viewportScaled = page.getViewport(scale),
+                  context        = canvas.getContext('2d'),
+                  renderContext = {
+                    canvasContext: context,
+                    viewport:      viewportScaled
+                  };
+
+              canvas.height = viewportScaled.height;
+              canvas.width  = viewportScaled.width;
+
+              page.render(renderContext);
+
+              if (pageNum  < pageCount) {
+                getPages(pageNum + 1, pageCount);
+              }
+            });
+          };
+
+          var cleanCanvas = function(){
+            var parentElement = element[0];
+            while (parentElement.firstChild) {
+              parentElement.removeChild(parentElement.firstChild);
+            }
+          };
+
+          cleanCanvas();
+
+          getPages(1, pdf.numPages);
+        });
+      });
+    }
+  };
 }]);
